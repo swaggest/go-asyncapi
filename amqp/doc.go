9a80497a3b506af92bddae2c05cbb756ec9da94e
Viacheslav Poturaev <vearutop@gmail.com>
2019-10-22 14:51:05 +0200

    Add package docs

---
+++ amqp/doc.go
@@ -0,0 +1,2 @@
+// Package amqp provides a helper to add AMQP information to AsyncAPI 1.2.0 schema.
+package amqp
---
+++ swgen/asyncapi-2.0.0/doc.go
@@ -0,0 +1,2 @@
+// Package asyncapi implements reflection-based AsyncAPI 2.0.0 schema generator.
+package asyncapi
