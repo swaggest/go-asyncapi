98b745b450cc91af6efcc6571872ebf71417c288
Viacheslav Poturaev <vearutop@gmail.com>
2022-09-23 09:48:56 +0200

    Use contextual schema in bindings

---
+++ resources/schema/prepare_bindings.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+find ./bindings/ -name .git -prune -o -type f -not -name prepare_bindings.sh -print0 | xargs -0 perl -i -pe "s|https://raw.githubusercontent.com/asyncapi/asyncapi-node/v2.7.7/schemas/2.0.0.json#/definitions/schema|http://asyncapi.com/definitions/2.4.0/schema.json|g"
+find ./bindings/ -name .git -prune -o -type f -not -name prepare_bindings.sh -print0 | xargs -0 perl -i -pe "s|https://raw.githubusercontent.com/asyncapi/asyncapi-node/v2.7.7/schemas/2.0.0.json#/definitions/specificationExtension|http://asyncapi.com/definitions/2.4.0/specificationExtension.json|g"
+find ./bindings/ -name .git -prune -o -type f -not -name prepare_bindings.sh -print0 | xargs -0 perl -i -pe 's|"title"|"x-title"|g'
+
+
+
